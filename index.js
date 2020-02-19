1190f01ddb0ae12e5459d0986dd735857e6da485
Adik <a_korabayev@mail.ru>
2020-02-19 09:53:46 +0600

    routing

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ app.get('', (req, res) => {
   res.send('<h1>Hello There</h1>');
 });
 
+app.get('/about', (req, res) => {
+  res.send('<h1>About page</h1>');
+});
+
+app.get('/help', (res, req) => {
+  res.send('<h1>Help page</h1>');
+})
+
 app.listen(port, () => console.log('port ', port))
